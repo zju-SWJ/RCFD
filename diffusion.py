6fbfbeddbce2a1d288866c474bccc2dfda2d30fb
Wujie Sun <103396396+zju-SWJ@users.noreply.github.com>
2023-05-15 13:45:13 +0800

    Update diffusion.py

--- diffusion.py
+++ diffusion.py
@@ -204,11 +204,13 @@ class GaussianDiffusionSampler(nn.Module):
         
     def ddpm(self, x_T, clip=True, y=-1): # DDPM sampler with stride 1, NOT used in our paper, NOT fully tested
         x_t = x_T
-        for time_step in reversed(range(1, self.T + 1)):
+        for time_step in reversed(range(0, self.T + 1)): 
+            # it actually uses self.T+1 steps
+            # if reversed(range(0, self.T)) is used, sampling may go wrong when self.T is small
             t = x_t.new_ones([x_T.shape[0], ], dtype=torch.long) * time_step
             mean, log_var = self.p_mean_variance(x_t=x_t, t=t, clip=clip, y=y)
-            # no noise when t == 1
-            if time_step > 1:
+            # no noise when t == 0
+            if time_step > 0:
                 noise = torch.randn_like(x_t)
             else:
                 noise = 0
